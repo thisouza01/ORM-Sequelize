f5cc484228d5f2edc2321df388512394523f1fb0
thisouza01 <tihsouza01@gmail.com>
2023-08-30 20:48:56 -0300

    adicionando controladores e populando novas tabelas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -8,6 +8,48 @@ class PessoaController {
         } catch (err) {
             return res.status(500).json(err.message)
         }
+    };
+
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            const umaPessoa = await database.Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(umaPessoa);
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    };
+
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body;
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(novaPessoaCriada)
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    };
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(pessoaAtualizada)
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    };
+
+    static async apagaPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } });
+            return res.status(200).json({ mensagem: `id ${ id } deletado` })
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
     }
 };
 
