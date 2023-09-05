30915a3e36d3372533c046142272fe0f3b34f943
thisouza01 <tihsouza01@gmail.com>
2023-09-05 20:55:47 -0300

    escopos padrao e validaçao no sequelize

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -22,12 +22,27 @@ module.exports = (sequelize, DataTypes) => {
   Pessoas.init({
     nome: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dados do tipo e-mail inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Pessoas',
-    paranoid: true
+    paranoid: true,
+    defaultScope: {
+      where: { ativo: true }
+    },
+    scopes: {
+      todos: { where: {} },
+      // caso precise de mais, só adicionar
+    }
   });
   return Pessoas;
 };
